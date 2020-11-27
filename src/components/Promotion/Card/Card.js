80e1228ec7720429a1196a83229d65caba9e83e9
Sara01romao <sara-romao@live.com>
2020-11-27 00:36:06 -0300

    .

--- src/components/Promotion/Card/Card.js
+++ src/components/Promotion/Card/Card.js
@@ -13,7 +13,10 @@ const PromotionCard = ({promotion}) =>(
           <div className="promotion-card__comment">"{promotion.comments[0].comment}"</div>
         )}
 
-        <div className="promotion-card__comments-count">{promotion.comments.length} Comentários</div>
+        <div className="promotion-card__comments-count">
+        {promotion.comments.length}{''}
+        {promotion.comments.length > 1 ? 'Comentários': 'Comentário'}
+        </div>
         <a href={promotion.url} target="_black" className="promotion-card__link">IR PARA O SITE</a>
       </footer>
     </div>
