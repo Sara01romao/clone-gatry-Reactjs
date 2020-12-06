e7d3accf7be284688f370f7176d4d4f89eb32192
Sara Romao <sara-romao@live.com>
2020-12-05 23:38:07 -0300

    react router

---
+++ src/pages/Root.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+//implementa a configução 11:50
+
+const Root =()=> {
+  return(
+    <Router>
+      <Switch>
+        <Route path="/" component=[]/>
+      </Switch>
+    </Router>
+  )
+}
